15a82a090546dca04971de27f1d44aefcafa0290
meindert hoving <meindert@shuntyard.co.za>
2013-01-07 10:32:58 +0200

    chnaged webrick config

--- config/application.rb
+++ config/application.rb
@@ -58,5 +58,9 @@ module Benchmark
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+
+    config.assets.logger = false
+    config.assets.logger = nil
+
   end
 end

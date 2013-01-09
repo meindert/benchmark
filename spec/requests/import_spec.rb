5dfa3428e9876e0de0195b94b0577a72e07ea8df
meindert hoving <meindert@shuntyard.co.za>
2013-01-09 09:37:20 +0200

    Upload file test

--- spec/requests/import_spec.rb
+++ spec/requests/import_spec.rb
@@ -2,9 +2,19 @@ require 'spec_helper'
 
 describe "POST create" do
 
-  it "should upload an image" do
+  before :each do
+    @file = fixture_file_upload('spec/support/CHRYSLER.csv', 'text/csv')
+  end
+
+  it "can upload a license" do
+    pending "add some examples to (or delete) #{__FILE__}"
+   # post :uploadLicense, :upload => @file
+   # response.should be_success
+  end
+
+  #it "should upload an image" do
    # Image.delete_all
-   uploader = mock_uploader 'spec/support/CHRYSLER.csv'
+   #uploader = mock_uploader 'spec/support/CHRYSLER.csv'
    # post :update, {
    #     :id => @object.id,
    #     :wibble => { :image_file => uploader }
@@ -14,20 +24,20 @@ describe "POST create" do
    # i = Image.find(:first)
    # i.filename.should == uploader.original_path
    # i.contents.length.should == uploader.size
-  end
+  #end
 
 
-  def mock_uploader(file, type = 'text/csv')
-    filename = "%s/%s" % [ File.dirname(__FILE__), file ]
-    uploader = ActionController::UploadedStringIO.new
-    uploader.original_path = filename
-    uploader.content_type = type
-    def uploader.read
-      File.read(original_path)
-    end
-    def uploader.size
-      File.stat(original_path).size
-    end
-    uploader
-  end
+  #def mock_uploader(file, type = 'text/csv')
+  #  filename = "%s/%s" % [ File.dirname(__FILE__), file ]
+  #  uploader = ActionController::UploadedStringIO.new
+  #  uploader.original_path = filename
+  #  uploader.content_type = type
+  ##  def uploader.read
+   #   File.read(original_path)
+   # end
+   # def uploader.size
+   #   File.stat(original_path).size
+   # end
+   # uploader
+  #e#nd
 end
